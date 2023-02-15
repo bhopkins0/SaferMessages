5233e1f7a49ffb59bc4a980f0cd844037826ba03
Brent Hopkins <contact@brenthopkins.me>
2023-02-15 17:17:01 -0600

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -5,3 +5,13 @@ CREATE TABLE `users` (
 	PRIMARY KEY (acc_id)
 );
 
+CREATE TABLE `messages` (
+	`message_id` INT NOT NULL AUTO_INCREMENT,
+	`user_from` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`user_to` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`message_password` VARCHAR(75) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`message_iv` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`message_tag` VARCHAR(24) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`message` VARCHAR(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	PRIMARY KEY (message_id)
+);
